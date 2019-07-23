0a974d6adc7aef67e8169f060fdccc51060ccdf0
Cristina Silva <crissilva.eng@gmail.com>
2019-07-23 01:11:17 -0300

    feat: add recommender source code with performance evaluation

--- src/main/scala/recommender/Recommender.scala
+++ src/main/scala/recommender/Recommender.scala
@@ -1,5 +1,36 @@
 package recommender
 
+import java.io._
+
+import org.apache.mahout.common._
+import org.apache.mahout.cf.taste.eval._
+import org.apache.mahout.cf.taste.model._
+import org.apache.mahout.cf.taste.impl.eval._
+import org.apache.mahout.cf.taste.recommender._
+import org.apache.mahout.cf.taste.impl.model.file._
+import org.apache.mahout.cf.taste.impl.similarity._
+import org.apache.mahout.cf.taste.impl.recommender._
+import org.apache.mahout.cf.taste.impl.neighborhood._
+
 object Recommender {
-    def main(args: Array[String]): Unit = println("not implemented")
+    
+    RandomUtils.useTestSeed()
+
+    class RecomendatorBuilder extends RecommenderBuilder {
+        def buildRecommender(model: DataModel) : Recommender = {
+            val similarity = new PearsonCorrelationSimilarity(model)
+            val neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model)
+            return new GenericUserBasedRecommender(model, neighborhood, similarity)
+        }
+    }
+
+    def main(args: Array[String]): Unit = {
+        val file = new File(args(0))
+        val model = new FileDataModel(file)
+
+        val evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator()
+        val builder = new RecomendatorBuilder()
+
+        println(evaluator.evaluate(builder, null, model, 0.8, 1.0))
+    }
 }
